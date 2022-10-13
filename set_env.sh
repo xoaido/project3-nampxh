20b0ca34de03aa6d323c7d2288a76ae3b234d720
xoaido <nambeo246@gmail.com>
2022-10-13 09:29:55 +0700

    first commit

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=nampxh
+export POSTGRES_PASSWORD=24062001
+export POSTGRES_HOST=project3-nampxh.cfgpprzkw2um.ap-southeast-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_BUCKET=project3-nampxh
+export AWS_REGION=ap-southeast-1
+export AWS_PROFILE=
 export JWT_SECRET=testing
 export URL=http://localhost:8100
